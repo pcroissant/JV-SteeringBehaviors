90b84d9870aab0a42d1cc9066e66b28c1a195491
Pierrick Croissant <pcroissant@ensc.fr>
2021-06-26 13:09:21 +0200

    refacto: linting

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from agent import Agent
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -12,37 +13,37 @@ class Game:
         self.clock = pygame.time.Clock()
         self.ticks = 60
         self.exit = False
-        
+
         self.group = [Agent(self.width, self.height) for i in range(100)]
-    
+
     def update(self):
-        
+
         while not self.exit:
-            
+
             # Update delta
             dt = self.clock.get_time() / 1000
-            
+
             # Check for event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.exit = True
 
             # Render
-            self.screen.fill((56,56,56))
-            
+            self.screen.fill((56, 56, 56))
+
             for agent in self.group:
                 agent.out_of_bounds(self.width, self.height)
                 agent.follow(self.group)
                 agent.update(dt)
-                
-                pygame.draw.circle(self.screen, (255,255,255), agent.position, 7)
-                
+
+                pygame.draw.circle(self.screen, (255, 255, 255), agent.position, 7)
+
             pygame.display.flip()
             self.clock.tick(self.ticks)
-        
-        pygame.quit()
-            
 
-if __name__ == '__main__':
+        pygame.quit()
+
+
+if __name__ == "__main__":
     game = Game()
     game.update()
